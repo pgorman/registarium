1fb888ed1be0ab1613a784f9e3a010b0b70a3835
Paul Gorman <gorman.paul@gmail.com>
2019-04-14 17:31:32 -0400

    Saving to SQLite works.

--- simpleinventory.go
+++ simpleinventory.go
@@ -16,16 +16,23 @@ import (
 	"github.com/bvinc/go-sqlite-lite/sqlite3"
 )
 
+var addr string
+var port string
 var clientKey string
+var charLimit int
 var dbFile string
 
 type hello struct {
 	ClientKey string `clientKey`
+	Hardware  string `json:hardware`
 	IP        string `json:ip`
+	FirstSeen string `json:firstSeen`
 	LastSeen  string `json:lastSeen`
 	MAC       string `json:mac`
-	Uname     string `json:uname`  // Output of `uname -a`
-	Uptime    string `json:uptime` // Output of `uptime`
+	NodeName  string `json:nodeName`
+	OSRel     string `json:osRel`
+	OSSys     string `json:osSys`
+	OSVer     string `json:osVer`
 	Hello     string `json:hello`
 }
 
@@ -42,12 +49,21 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println(h)
-	conn, err := sqlite3.Open(dbFile)
+	db, err := sqlite3.Open(dbFile)
 	if err != nil {
 		log.Println(err)
 	}
-	defer conn.Close()
-	conn.BusyTimeout(5 * time.Second)
+	defer db.Close()
+	db.BusyTimeout(5 * time.Second)
+	err = db.Exec(`INSERT INTO hellos
+		(mac, firstSeen, hardware, ip, lastSeen, nodeName, osRel, osSys, osVer, hello)
+		values (?, CURRENT_TIMESTAMP, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?, ?, ?)
+		ON CONFLICT(mac) DO UPDATE SET
+		ip=?, lastSeen=CURRENT_TIMESTAMP, nodeName=?, osRel=?, osSys=?, osVer=?, hello=?`,
+		h.MAC, h.Hardware, h.IP, h.NodeName, h.OSRel, h.OSSys, h.OSVer, h.Hello, h.IP, h.NodeName, h.OSRel, h.OSSys, h.OSVer, h.Hello)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func init() {
@@ -56,26 +72,36 @@ func init() {
 		log.Fatal("please set the 'clientKey' environment variable")
 	}
 
-	var addr, port string
 	flag.StringVar(&addr, "address", "127.0.0.1", "network address where we server API")
 	flag.StringVar(&port, "port", "9753", "network port to serve API")
 	flag.StringVar(&dbFile, "db", "simpleinventory.db", "SQLite database file")
+	flag.IntVar(&charLimit, "char-limit", 99, "truncate JSON values supplied by clients at this limit")
 	flag.Parse()
 
 	_, err := os.Stat(dbFile)
 	if err != nil {
 		log.Println("database file", dbFile, "doesn't already exist")
-		conn, err := sqlite3.Open(dbFile)
+		db, err := sqlite3.Open(dbFile)
 		if err != nil {
 			log.Println(err)
 		}
-		defer conn.Close()
-		conn.BusyTimeout(5 * time.Second)
-		/* TODO
-		   We should probably split some of these into different database fields.
-		   E.g., extract the hostname from uptime.
+		defer db.Close()
+		db.BusyTimeout(5 * time.Second)
+		/*
+			Possible sources of values:
+			mac= TODO
+			hardware=uname -m
+			ip (Linux)=ip route get $(ip route show | grep default | awk '{ print $3 }') | grep src | awk '{ print $5 }'
+			ip (OpenBSD)=route -n get $(route -n show | grep default | awk '{ print $2 }') | grep 'if address' | awk '{ print $3 }'
+			nodeName=uname -n
+			osSys=uname -s
+			osRel=uname -r
+			osVer=uname -v
 		*/
-		err = conn.Exec(`CREATE TABLE IF NOT EXISTS hellos (mac CHARACTER(18) PRIMARY KEY, ip VARCHAR(70), firstseen DATETIME, lastseen DATETIME, uname VARCAR(255), uptime VARCHAR(255), hello TEXT)`)
+		err = db.Exec(`CREATE TABLE IF NOT EXISTS hellos
+			(mac TEXT PRIMARY KEY, firstseen NUMERIC, hardware TEXT, ip TEXT,
+			lastseen NUMERIC, nodename TEXT, osrel TEXT, ossys TEXT,
+			osver TEXT, hello TEXT)`)
 		if err != nil {
 			log.Println(err)
 		}
--- testcurl.sh
+++ testcurl.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -euf
+
+clientKey=secret398726978clientkey
+ip=$(ip route get $(ip route show | grep default | awk '{ print $3 }') | grep src | awk '{ print $5 }')
+mac=$(ip address show up | grep -B 1 "$ip" | head -n 1 | awk '{ print $2 }')
+nodeName=$(uname -n)
+osSys=$(uname -s)
+osRel=$(uname -r)
+osVer=$(uname -v)
+hello="I'm a thin client!"
+json=$(printf '{"clientKey":"%s","ip":"%s","mac":"%s", "hello":"%s"}' "$clientKey" "$ip" "$mac" "$hello")
+
 curl --header "Content-Type: applicaton/json" \
 	--request POST \
-	--data '{"clientKey":"secret398726978clientkey","ip":"192.0.1.222","mac":"x2:01:ab:23:cd:45:ef:67}","uname":"Linux falstaff 4.9.0-8-amd64 #1 SMP Debian 4.9.144-3.1 (2019-02-19) x86_64 GNU/Linux","uptime":" 15:15:22 up 8 days,  2:24, 20 users,  load average: 0.01, 0.04, 0.04","hello":"I am a thin client"'} \
+	--data "$json" \
 	http://localhost:9753/api/v1/hello
